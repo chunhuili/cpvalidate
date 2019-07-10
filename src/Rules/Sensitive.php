d25d181e2d50ca5a78048ba5a2e2e080446881e2
chunhuili <2947680759@qq.com>
2019-07-10 09:27:04 +0800

    fix 增加敏感词

---
+++ src/Rules/Sensitive.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lichunhui
+ * Date: 2019/7/10
+ * Time: 上午9:14
+ */
+
+namespace Chunhuili\Cpvalidate\Rules;
+
+use Chunhuili\Cpvalidate\Rule;
+
+class Sensitive extends Rule
+{
+    /** @var string */
+    protected $message = "this is Sensitive word";
+
+    /**
+     * Check the $value is valid
+     *
+     * @param mixed $value
+     * @return bool
+     */
+    public function check($value): bool
+    {
+        $sensitiveWord = [];
+
+        if (in_array($value,$sensitiveWord)) {
+            return false;
+        }
+
+        return true;
+    }
+}
